147e96ca98504aab3ecdc382675bb208d452feef
Tobin Frost <habib92m@gmail.com>
2022-02-16 17:41:51 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Kalpay Gateway Doc'
+copyright = '2021, Kalpay'
+author = 'Tobin Frost'
 
 release = '0.1'
 version = '0.1.0'
